41ef50c48d68a62deaafc12e4f36efff940a7ed5
Ishan Gaurav <ishangaurav278@gmail.com>
2026-01-24 19:05:09 +0530

    edited footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -77,9 +77,9 @@ const Footer = () => {
                     <h2 className='text-base font-medium text-gray-800 uppercase'>Contact</h2>
                     <ul className='mt-3 flex flex-col gap-1.5'>
                         <li>1234 Luxury Drive</li>
-                        <li>San Francisco, CA 94107</li>
-                        <li>+1 234 567890</li>
-                        <li>info@example.com</li>
+                        <li>BIT Mesra , Ranchi</li>
+                        <li>+91 ----------</li>
+                        <li>ishangaurav999@gmail.com</li>
                     </ul>
                 </div>
 
